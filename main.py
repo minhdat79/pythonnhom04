a9c3c0aa37fac01667d8e34b51ef5ed7b85c77d4
minhdat79 <dat.vminh07@gmail.com>
2024-10-04 11:14:52 +0700

    dat

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame, sys
+from spaceship import Spaceship
+
+pygame.init()
+
+SCREEN_WIDTH = 750
+SCREEN_HEIGHT = 700
+
+GREY = (29, 29, 27)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Python Space Invader")
+
+clock = pygame.time.Clock()
+
+
+spaceship = Spaceship(SCREEN_WIDTH, SCREEN_HEIGHT)
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    spaceship_group.update()
+
+
+    screen.fill(GREY)
+    spaceship_group.draw(screen)
+    spaceship_group.sprite.lasers_group.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
