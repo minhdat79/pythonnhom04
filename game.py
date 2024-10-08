d21490c6ec9dd0b4aca6547fec5978483c2ea398
Cauam3328 <163101390+Cauam3328@users.noreply.github.com>
2024-10-08 12:20:39 +0700

    vi
    i'm done

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
-import pygame
-import random
+import pygame, random
 from spaceship import Spaceship
 from obstacle import Obstacle, grid
-
+from Graphics.alien import Alien
+from laser import Laser
 class Game:
     def __init__(self, screen_width, screen_height):
         self.screen_width = screen_width
@@ -12,6 +12,10 @@ class Game:
         self.obstacle_width = len(grid[0]) * 3  # Assuming grid[0] defines the width of one obstacle
         self.num_obstacles = 4  # Number of obstacles in a row
         self.obstacles = self.create_obstacles()
+        self.aliens_group = pygame.sprite.Group()
+        self.create_aliens()
+        self.aliens_direction = 1
+        self.alien_lasers_group = pygame.sprite.Group()
 
     def create_obstacles(self):
         # Calculate the total space used by all obstacles plus the gaps
@@ -26,3 +30,41 @@ class Game:
             obstacles.append(obstacle)
 
         return obstacles
+    def create_aliens(self):
+        for row in range(5):
+            for column in range(11):
+                x = 75 + column * 55
+                y = 110 + row * 55
+
+                if row == 0:
+                    alien_type = 3
+                elif row in (1,2):
+                    alien_type = 2
+                else:
+                    alien_type =1
+
+                alien = Alien (alien_type, x, y)
+                self.aliens_group.add(alien)
+
+    def move_aliens (self):
+        self.aliens_group.update(self.aliens_direction)
+
+        alien_sprites = self.aliens_group.sprites()
+        for alien in alien_sprites:
+            if alien.rect.right >= self.screen_width:
+                self.aliens_direction = -1
+                self.alien_move_down(2)
+            elif alien.rect.left <=0:
+                self.aliens_direction = 1
+                self.alien_move_down(2)
+
+    def alien_move_down(self, distance):
+        if self.aliens_group:
+            for alien in self.aliens_group.sprites():
+                alien.rect.y += distance
+
+    def alien_shoot_laser(self):
+        if self.aliens_group.sprites():
+            random_alien = random.choice(self.aliens_group.sprites())
+            laser_sprite = Laser(random_alien.rect.center, -6, self.screen_height)
+            self.alien_lasers_group.add(laser_sprite)
--- main.py
+++ main.py
@@ -13,17 +13,31 @@ pygame.display.set_caption("Python Space Invader")
 clock = pygame.time.Clock()
 
 game = Game(SCREEN_WIDTH,SCREEN_HEIGHT)
+
+SHOOT_LASER = pygame.USEREVENT
+pygame.time.set_timer(SHOOT_LASER, 50)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SHOOT_LASER:
+            game.alien_shoot_laser()
 
+
+#updating
     game.spaceship_group.update()
+    game.move_aliens()
+
+    game.alien_lasers_group.update()
+
     screen.fill(GREY)
     game.spaceship_group.draw(screen)
     game.spaceship_group.sprite.lasers_group.draw(screen)
     for obstacle in game.obstacles:
         obstacle.blocks_group.draw(screen)
+    game.aliens_group.draw(screen)
+    game.alien_lasers_group.draw(screen)
+
     pygame.display.update()
     clock.tick(60)
