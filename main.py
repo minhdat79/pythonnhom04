8e70a93c2490def7d45b92c43a12b7b54a896282
EyelessJack0903 <eyelessjack0903@gmail.com>
2024-10-09 08:24:24 +0700

    update done

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ YELLOW = (243, 216, 63)
 font = pygame.font.Font("Font/monogram.ttf", 40)
 level_surface = font.render("LEVEL 01", False, YELLOW)
 game_over_surface = font.render("GAME OVER", False, YELLOW)
+screen_text_surface = font.render("SCORE", False, YELLOW)
+highscore_text_surface = font.render("HIGH SCORE", False, YELLOW)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH + OFFSET, SCREEN_HEIGHT + 2*OFFSET))
 pygame.display.set_caption("Python Space Invader")
@@ -68,6 +70,14 @@ while True:
         screen.blit(game.spaceship_group.sprite.image, (x, 745))
         x += 50
 
+    screen.blit(screen_text_surface, (50,15,50,50))
+    formatted_score = str(game.score).zfill(5)
+    score_surface = font.render(formatted_score,False,YELLOW)
+    screen.blit(score_surface, (50,40,50,50))
+    screen.blit(highscore_text_surface,(550,15,50,50))
+    formatted_highscore = str(game.highscore).zfill(5)
+    highscore_surface = font.render(formatted_highscore, False, YELLOW)
+    screen.blit(highscore_surface, (625,40,50,50))
 
     game.spaceship_group.draw(screen)
     game.spaceship_group.sprite.lasers_group.draw(screen)
